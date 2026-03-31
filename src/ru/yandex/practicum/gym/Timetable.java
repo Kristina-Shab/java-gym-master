4f088cffe0df63e52d23c5003555a0b944206d46
Kristina Shabelskaya <krmatveeva@yandex.ru>
2026-03-31 16:57:49 +0300

    Реализовать основную логику класса Timetable

--- src/ru/yandex/practicum/gym/Timetable.java
+++ src/ru/yandex/practicum/gym/Timetable.java
@@ -4,17 +4,38 @@ import java.util.*;
 
 public class Timetable {
 
-    private /* как это хранить??? */ timetable;
+    private final Map<DayOfWeek, Map<TimeOfDay, List<TrainingSession>>> timetable = new HashMap<>();
 
     public void addNewTrainingSession(TrainingSession trainingSession) {
-        //сохраняем занятие в расписании
+        DayOfWeek day = trainingSession.getDayOfWeek();
+        TimeOfDay time = trainingSession.getTimeOfDay();
+        Map<TimeOfDay, List<TrainingSession>> dailySchedule = getOrCreateDailySchedule(day);
+        List<TrainingSession> sessionsAtTime = getOrCreateTimeSlot(dailySchedule, time);
+        sessionsAtTime.add(trainingSession);
     }
 
-    public /* непонятно, что возвращать */ getTrainingSessionsForDay(DayOfWeek dayOfWeek) {
-        //как реализовать, тоже непонятно, но сложность должна быть О(1)
+    private Map<TimeOfDay, List<TrainingSession>> getOrCreateDailySchedule(DayOfWeek day) {
+        if (!timetable.containsKey(day)) {
+            timetable.put(day, new TreeMap<>());
+        }
+        return timetable.get(day);
     }
 
-    public /* непонятно, что возвращать */ getTrainingSessionsForDayAndTime(DayOfWeek dayOfWeek, TimeOfDay timeOfDay) {
-        //как реализовать, тоже непонятно, но сложность должна быть О(1)
+    private List<TrainingSession> getOrCreateTimeSlot(
+            Map<TimeOfDay, List<TrainingSession>> dailySchedule,
+            TimeOfDay time
+    ) {
+        if (!dailySchedule.containsKey(time)) {
+            dailySchedule.put(time, new ArrayList<>());
+        }
+        return dailySchedule.get(time);
+    }
+
+    public Map<TimeOfDay, List<TrainingSession>> getTrainingSessionsForDay(DayOfWeek dayOfWeek) {
+        return timetable.get(dayOfWeek);
+    }
+
+    public List<TrainingSession> getTrainingSessionsForDayAndTime(DayOfWeek dayOfWeek, TimeOfDay timeOfDay) {
+        return timetable.get(dayOfWeek).get(timeOfDay);
     }
 }
