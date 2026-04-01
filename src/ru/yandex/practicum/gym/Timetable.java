91cf910a97b814dc4298b0e6963914e2c39f716c
Kristina Shabelskaya <krmatveeva@yandex.ru>
2026-04-02 00:47:26 +0300

    Добавить функцию подсчета тренировок для каждого тренера

--- src/ru/yandex/practicum/gym/Timetable.java
+++ src/ru/yandex/practicum/gym/Timetable.java
@@ -3,8 +3,8 @@ package ru.yandex.practicum.gym;
 import java.util.*;
 
 public class Timetable {
-
     private final Map<DayOfWeek, Map<TimeOfDay, List<TrainingSession>>> timetable = new HashMap<>();
+    private final Map<Coach, Integer> counter = new HashMap<>();
 
     public void addNewTrainingSession(TrainingSession trainingSession) {
         DayOfWeek day = trainingSession.getDayOfWeek();
@@ -38,4 +38,45 @@ public class Timetable {
     public List<TrainingSession> getTrainingSessionsForDayAndTime(DayOfWeek dayOfWeek, TimeOfDay timeOfDay) {
         return timetable.get(dayOfWeek).get(timeOfDay);
     }
+
+    public List<CounterOfTrainings> getCountByCoaches() {
+        Map<Coach, Integer> coaches = addAllSessionsToCounter();
+        List<CounterOfTrainings> counterOfTrainingsList = mapToCounterOfTrainingList(coaches);
+        Collections.sort(counterOfTrainingsList);
+        return counterOfTrainingsList;
+    }
+
+    private List<CounterOfTrainings> mapToCounterOfTrainingList(Map<Coach, Integer> coaches) {
+        List<CounterOfTrainings> counterOfTrainingsList = new ArrayList<>();
+        for (Map.Entry<Coach, Integer> coach : coaches.entrySet()) {
+            CounterOfTrainings counterOfTrainings = new CounterOfTrainings(coach.getKey(), coach.getValue());
+            counterOfTrainingsList.add(counterOfTrainings);
+        }
+        return counterOfTrainingsList;
+    }
+
+    private Map<Coach, Integer> addAllSessionsToCounter() {
+        for (Map.Entry<DayOfWeek, Map<TimeOfDay, List<TrainingSession>>> day : timetable.entrySet()) {
+            Map<TimeOfDay, List<TrainingSession>> times = day.getValue();
+            for (Map.Entry<TimeOfDay, List<TrainingSession>> time : times.entrySet()) {
+                List<TrainingSession> sessions = time.getValue();
+                for (TrainingSession session : sessions) {
+                    addSessionToCounter(session);
+                }
+            }
+        }
+        return counter;
+    }
+
+    private void addSessionToCounter(TrainingSession session) {
+        Coach coach = session.getCoach();
+        if (counter.containsKey(coach)) {
+            int count = counter.get(coach);
+            count++;
+            counter.put(coach, count);
+        } else {
+            counter.put(coach, 1);
+        }
+    }
+
 }
